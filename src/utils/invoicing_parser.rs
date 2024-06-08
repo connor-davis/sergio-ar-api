6fa03049e0740cf35c0991c441445ae01e596fd1
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-06-08 13:12:02 +0200

    Fixes

--- src/utils/invoicing_parser.rs
+++ src/utils/invoicing_parser.rs
@@ -10,12 +10,16 @@ pub struct InvoicingParser {}
 impl InvoicingParser {
     pub async fn parse_invoicing_file(file_path: &str) -> Result<String, Error> {
         // Open the csv file.
+        println!("❕ Opening file {}", file_path);
         let mut file = File::open(file_path)?;
         let mut lines = String::new();
 
+        println!("❕ Reading file to string.");
         file.read_to_string(&mut lines)?;
         file.flush()?;
+        println!("❕ Done");
 
+        println!("❕ Replacing tab stops.");
         let lines = lines.replace("\t", ",");
 
         println!("{:?}", lines);
