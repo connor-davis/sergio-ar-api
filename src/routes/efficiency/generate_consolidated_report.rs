f1a38484a3540f894d5cd1231463e68299abace5
Connor Richard Davis <st10068305@vcconnect.edu.za>
2024-06-11 11:31:49 +0200

    Updates

--- src/routes/efficiency/generate_consolidated_report.rs
+++ src/routes/efficiency/generate_consolidated_report.rs
@@ -143,10 +143,7 @@ pub async fn generate_consolidated_report(
             match schedule.shift_type.as_str() {
                 "Pickup" => picked_up += 1,
                 "Internal Pickup" => internal_picked_up += 1,
-                "Dropped & Picked Up" => {
-                    picked_up += 1;
-                    total_dropped += 1;
-                }
+                "Dropped & Picked Up" => dropped += 1,
                 "Dropped" => dropped += 1,
                 _ => {}
             }
